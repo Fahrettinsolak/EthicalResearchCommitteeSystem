97a9b7315b74c8fbe5f7c9cb8a5753c26fded99b
Ramazan Serhat Uygun <rsuygun@outlook.com>
2025-03-26 16:33:05 +0300

    Dockerfile dosyaları yazıldı

--- Services/NotificationService/ERCS.NotificationService/Program.cs
+++ Services/NotificationService/ERCS.NotificationService/Program.cs
@@ -7,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5004); // Uygulama 5004 portunda çalýþýr
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
