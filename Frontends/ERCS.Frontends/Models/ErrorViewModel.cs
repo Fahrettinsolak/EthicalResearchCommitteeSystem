be1612f3ad49411fd3c6c986e29363e71d99f8ac
Ramazan Serhat Uygun <rsuygun@outlook.com>
2025-03-26 13:18:35 +0300

    Database ve Frontend klasör yapısı kuruldu

---
+++ Frontends/ERCS.Frontends/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ERCS.Frontends.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
